685ea7481984ca7ef324fcf90f40d6f8149c6e0f
Anand Tasgave <tasgaveanand18@gmail.com>
2025-01-26 16:59:43 +0530

    some_change

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify # type: ignore
 import json
-import requests
 
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
